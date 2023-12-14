e9b627f57e6c3c4f49675e65ff3640d4ef025928
behzad <bsd098@gmail.com>
2023-12-14 10:17:00 +0330

    implement scroll nav links

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,16 @@ $(function () {
     $('.nav-menu-icon').on('click', function (){
         $('.nav-links').toggleClass('show-links')
     })
+
+})
+
+$(function(){
+    $('a.scroll-link').on('click',function(event){
+        event.preventDefault();
+        $('.nav-links').removeClass('show-links');
+        let sectionId = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(sectionId).offset().top
+        }, 1000);
+    })
 })
